15d2d47759f7a34db729866754b9cef9499cd987
Abhishek Raina <abhishek.raina.uit@gmail.com>
2023-10-26 15:01:46 +0530

    Initial Commit

---
+++ lib/story.dart
@@ -0,0 +1,29 @@
+
+
+class Story {
+  late String storyTitle;
+  late String choice1;
+  late String choice2;
+
+  //Before- Seeing the other's code
+
+  // Story({required String storyTitle, required String choice1, required String choice2})
+  // {
+  //   this.storyTitle = storyTitle;
+  //   this.choice1 = choice1;
+  //   this.choice2 = choice2;
+  //
+  // }
+
+//After- Learned this from someone else's code
+  Story(
+      {required this.storyTitle, required this.choice1, required this.choice2});
+}
+
+
+
+//Done TODO: Step 2 - Create a new class called Story.
+
+//Done TODO: Step 3 - Create 3 properties for this class, A. storyTitle to store the story text. B. choice1 to store the text for choice 1, C. choice2 to store the text for choice 2.
+
+//Done TODO: Step 4 - Create a Constructor for this class to be able to initialise the properties created in step 3.
